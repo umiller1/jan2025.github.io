9be5447c4d17ac0af10794a6b82f35af2780b62e
Unique <unique.miller@first.edu>
2025-06-13 16:25:36 -0400

    Agenda

--- zagenda/js/audio.js
+++ zagenda/js/audio.js
@@ -1,5 +1,6 @@
 function playAudio() {
     let audioPlayer = document.getElementById('unique-audio');
-    audioPlayer.load();  // Reload the audio to ensure it plays from the beginning
+    // Reload the audio to ensure it plays from the
+    audioPlayer.load();
     audioPlayer.play();
 }
