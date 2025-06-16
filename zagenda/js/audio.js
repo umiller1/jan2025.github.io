ab1c75d22ed5d75892bf429579cb3a55afe9e75f
Unique <unique.miller@first.edu>
2025-06-16 08:53:20 -0400

    The boofs!

--- zagenda/js/audio.js
+++ zagenda/js/audio.js
@@ -1,6 +1,6 @@
 function playAudio() {
     let audioPlayer = document.getElementById('unique-audio');
-    // Reload the audio to ensure it plays from the
+    // Reload the audio to ensure it plays.
     audioPlayer.load();
     audioPlayer.play();
 }
